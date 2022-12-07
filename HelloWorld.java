6770c71b2f193d582bd8fb91c797370f12dac60c
Michael Iwanek <miwanek11199@gmail.com>
2022-12-07 08:42:38 -0800

    add existing files

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World!"); 
+
+        int num1 = 3;
+        int num2 = 10;
+        int sum = num1 + num2;
+
+        int test_num = 15;
+
+        System.out.println(sum); 
+        
+
+        if (sum > test_num) {
+            System.out.println("sum is greater than test_num");
+        }
+        else{
+            System.out.println("sum is less than test_num");
+        }
+
+
+    }
+}
