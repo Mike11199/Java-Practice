6b582e662fb42595564f923785bfc7ce60664338
Michael Iwanek <miwanek11199@gmail.com>
2022-12-07 08:59:50 -0800

    added things

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,15 +4,15 @@ class HelloWorld {
         
         System.out.println("Hello, World!"); 
 
+
+        // test some math
         int num1 = 3;
         int num2 = 10;
         int sum = num1 + num2;
-
         int test_num = 15;
-
         System.out.println(sum); 
         
-
+        // test if statement
         if (sum > test_num) {
             System.out.println("sum is greater than test_num");
         }
@@ -20,6 +20,15 @@ class HelloWorld {
             System.out.println("sum is less than test_num");
         }
 
+        // test using a function
+        int test_num_squared = squareNumber(test_num);
+        System.out.println(test_num_squared);
 
     }
+    public static int squareNumber(int x){
+
+        x = x*x;        
+        return x;
+    }
+
 }
